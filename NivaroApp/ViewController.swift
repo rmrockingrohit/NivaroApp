0abda0b822c123b83bc8e3c536049113e3894ab1
Durga Shanker Mishra <rmrockingrohit@gmail.com>
2021-05-11 20:47:40 +0530

    Initial Commit

---
+++ NivaroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NivaroApp
+//
+//  Created by Durga shankar Mishra on 11/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
